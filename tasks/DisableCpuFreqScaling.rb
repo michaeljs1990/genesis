86e8ee2ffc1139af928e88dcca8505463783eea4
Michael Schuett <michaeljs1990@gmail.com>
2017-05-29 13:31:08 -0400

    Refactor DisableCpuFreq
    Make sure we are closing files properly as well as checking to make
    sure that the changes we are making take effect.

--- tasks/DisableCpuFreqScaling.rb
+++ tasks/DisableCpuFreqScaling.rb
@@ -2,15 +2,25 @@ class DisableCpuFreqScaling
   include Genesis::Framework::Task
 
   init do
-    log "Make sure acpi-cpufreq is enabled" 
+    log "Make sure acpi-cpufreq is enabled"
     run_cmd('modprobe acpi-cpufreq')
   end
 
   run do
-    num_procs = run_cmd('cat /proc/cpuinfo | grep processor | wc -l')
-    (0..(num_procs.to_i - 1)).each do |n|
-      run_cmd("echo 'performance' > /sys/devices/system/cpu/cpu#{n}/cpufreq/scaling_governor")
+    cpu_dirs = '/sys/devices/system/cpu/cpu[0-9]*'
+    Dir.glob(cpu_dirs).each do |dirname|
+      File.open("#{dirname}/cpufreq/scaling_governor", 'w') { |f| f.write('performance') }
+    end
+
+    log 'Syncing files to disk and sleep 5'
+
+    # Verify CPUs set to max freq 
+    Dir.glob(cpu_dirs).each do |dirname|
+      max = File.read("#{dirname}/cpufreq/cpuinfo_max_freq").strip
+      cur = File.read("#{dirname}/cpufreq/cpuinfo_cur_freq").strip
+      raise "Max CPU frequency did not take" unless max == cur
     end
   end
 
 end
+
