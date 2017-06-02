fa79a76c77e7cf9406cf601640d9e8b8419be1ce
Michael Schuett <michaeljs1990@gmail.com>
2017-06-02 03:37:56 -0400

    Cleanup after review
    remove some redundancy and unneeded logging.

--- tasks/DisableCpuFreqScaling.rb
+++ tasks/DisableCpuFreqScaling.rb
@@ -6,20 +6,19 @@ class DisableCpuFreqScaling
   include Genesis::Framework::Task
 
   init do
-    log "Make sure acpi-cpufreq is enabled"
     run_cmd('modprobe acpi-cpufreq')
   end
 
   run do
-    cpu_dirs = '/sys/devices/system/cpu/cpu[0-9]*'
+    cpu_dirs = '/sys/devices/system/cpu/cpu[0-9]*/cpufreq'
     Dir.glob(cpu_dirs).each do |dirname|
-      File.write("#{dirname}/cpufreq/scaling_governor", 'performance')
+      File.write("#{dirname}/scaling_governor", 'performance')
     end
 
     # Verify CPUs set to max freq 
-    Dir.glob(cpu_dirs).each do |dirname|
-      max = File.read("#{dirname}/cpufreq/cpuinfo_max_freq").strip
-      cur = File.read("#{dirname}/cpufreq/cpuinfo_cur_freq").strip
+    Dir.glob("#{cpu_dirs}").each do |dirname|
+      max = File.read("#{dirname}/cpuinfo_max_freq").strip
+      cur = File.read("#{dirname}/cpuinfo_cur_freq").strip
       raise "Max CPU frequency did not take" unless max == cur
     end
   end
